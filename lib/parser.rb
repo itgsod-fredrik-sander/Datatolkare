67a2d3baff0496f95ec14446864ffe2b33011339
Fredrik Sander <fredde.sander@gmail.com>
2014-05-22 11:01:52 +0200

    Assigned weather hash with it keys and values

--- lib/parser.rb
+++ lib/parser.rb
@@ -8,5 +8,5 @@ end
 def encode_line(weather_data)
   raise ArgumentError, 'incomplete array' if weather_data.size < 3
 
-  weather = {}
+  weather = {date: weather_data[0], max: weather_data[1], min: weather_data[2]}
 end
