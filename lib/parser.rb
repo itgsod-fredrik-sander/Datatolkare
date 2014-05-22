60ca15264ce015adf90c1ec91ba84e210aa8b232
Fredrik Sander <fredde.sander@gmail.com>
2014-05-22 11:19:54 +0200

    Defined load_weather_file method

--- lib/parser.rb
+++ lib/parser.rb
@@ -16,3 +16,7 @@ def find_biggest_variation(weather_data)
   raise ArgumentError, 'array must not be empty' if weather_data.empty?
   return weather_data.max {|a, b| a[:max] - a[:min] <=> b[:max] - b[:min]}
 end
+
+def load_weather_file(filepath)
+
+end
